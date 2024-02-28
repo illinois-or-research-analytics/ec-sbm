174503a1e444dfa7e86a5aa5a8b09322c1ae1eb6
The-Anh Vu-Le <vltanh@illinois.edu>
2024-02-27 22:14:19 -0600

    move generated seqs in abcd to a new folder for testing modifications (does not have to regenerate the seqs)

---
+++ move_abcd.py
@@ -0,0 +1,19 @@
+import os
+
+src = 'abcd'
+dst = 'abcdta2'
+
+src_dir = f'data/networks/{src}/'
+dst_dir = src_dir.replace(src, dst)
+# os.makedirs(abcdta_dir, exist_ok=True)
+os.system(f'mkdir -p {dst_dir}')
+for d in os.listdir(src_dir):
+    src_subdir = f'{src_dir}/{d}'
+    dst_subdir = src_subdir.replace(src, dst)
+    os.system(f'mkdir -p {dst_subdir}')
+    # os.makedirs(abcdta_subdir, exist_ok=True)
+    for dd in os.listdir(src_subdir):
+        src_subsubdir = f'{src_subdir}/{dd}'
+        dst_subsubdir = src_subsubdir.replace(src, dst)
+        # os.makedirs(abcdta_subsubdir, exist_ok=True)
+        os.system(f'cp -r {src_subsubdir} {dst_subsubdir}')
