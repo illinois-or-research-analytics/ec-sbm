60ef84e9eec0d1b27a5c49f574ccf153a25c4792
The-Anh Vu-Le <vltanh@illinois.edu>
2024-03-05 21:55:44 -0600

    minor changes

--- gen_network.sh
+++ gen_network.sh
@@ -4,57 +4,57 @@
 #SBATCH --job-name="gen_network"
 #SBATCH --partition=tallis
 
-for method in abcd
-do
-    for network_id in cen wiki_topcats cit_patents cit_hepph wiki_talk oc
-    do
-        for resolution in .001 .01 .1
-        do
-            echo "=> $network_id at $resolution using $method <="
+# for method in abcdta2
+# do
+#     for network_id in cen #wiki_topcats cit_patents cit_hepph wiki_talk oc
+#     do
+#         for resolution in .1 #.001 .01 .1
+#         do
+#             echo "=> $network_id at $resolution using $method <="
 
-            echo "======"
+#             echo "======"
 
-            # echo "Generating..."
-            # python gen_abcdta.py $network_id $resolution $method
+#             echo "Generating..."
+#             python gen_abcdta.py $network_id $resolution $method
 
-            echo "======"
+#             echo "======"
 
-            echo "Computing statistics..."
-            python compute_stats.py $network_id $resolution $method
+#             echo "Computing statistics..."
+#             python compute_stats.py $network_id $resolution $method
 
-            echo "======"
+#             echo "======"
 
-            python emulate-real-nets/estimate_properties_networkit.py \
-                    -n "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/edge.dat" \
-                    -c "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/com.dat" \
-                    -o "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/stats.json"
+#             python emulate-real-nets/estimate_properties_networkit.py \
+#                     -n "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/edge.dat" \
+#                     -c "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/com.dat" \
+#                     -o "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/stats.json"
 
-            echo "======"
+#             echo "======"
 
-            python compute_upperbound.py $network_id $resolution $method
+#             python compute_upperbound.py $network_id $resolution $method
 
-            python compute_potential_connectivity.py $network_id $resolution $method
+#             python compute_potential_connectivity.py $network_id $resolution $method
 
-            python compute_wiring_efficiency.py $network_id $resolution $method
+#             python compute_wiring_efficiency.py $network_id $resolution $method
 
-            echo "==========================="
-            echo " "
-        done
-    done
-done
+#             echo "==========================="
+#             echo " "
+#         done
+#     done
+# done
 
 # ===================================
 
-# network_id=cen
-# resolution=.01
-# method=abcdta2
+network_id=cen
+resolution=.01
+method=abcdta2
 
-# python gen_abcdta.py $network_id $resolution $method
-# python compute_stats.py $network_id $resolution $method
-# python emulate-real-nets/estimate_properties_networkit.py \
-#     -n "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/edge.dat" \
-#     -c "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/com.dat" \
-#     -o "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/stats.json"
-# python compute_upperbound.py $network_id $resolution $method
-# python compute_potential_connectivity.py $network_id $resolution $method
-# python compute_wiring_efficiency.py $network_id $resolution $method
+python gen_abcdta.py $network_id $resolution $method
+python compute_stats.py $network_id $resolution $method
+python emulate-real-nets/estimate_properties_networkit.py \
+    -n "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/edge.dat" \
+    -c "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/com.dat" \
+    -o "data/networks/${method}/${network_id}_${method}_networks/${network_id}_leiden${resolution}_${method}/stats.json"
+python compute_upperbound.py $network_id $resolution $method
+python compute_potential_connectivity.py $network_id $resolution $method
+python compute_wiring_efficiency.py $network_id $resolution $method
