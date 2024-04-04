9df8b74f92e63908710666291a0d4e1a9ab0103c
The-Anh Vu-Le <vltanh@illinois.edu>
2024-04-04 07:37:35 -0500

    plot degree distribution to visualize the difference

--- stats.sh
+++ stats.sh
@@ -8,7 +8,7 @@ for method in abcd abcdta2
 do
     for based_on in leiden_cpm_cm #leiden_cpm_cm leiden_cpm
     do
-        for network_id in cit_hepph #cit_hepph cit_patents wiki_topcats wiki_talk orkut
+        for network_id in cit_patents #cit_hepph cit_patents wiki_topcats wiki_talk orkut
         do
             for resolution in .0001 .001 .01 #.0001 .001 .01
             do
@@ -20,9 +20,9 @@ done
 
 # ===================================
 
-# network_id=cit_hepph
-# resolution=.001
-# method=abcd
-# based_on=leiden_cpm_cm
+network_id=cit_hepph
+resolution=.0001
+method=abcd
+based_on=leiden_cpm_cm
 
-# python compute_degree_dist.py ${network_id} ${resolution} ${method} ${based_on}
+python compute_degree_dist.py ${network_id} ${resolution} ${method} ${based_on}
