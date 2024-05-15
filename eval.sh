15b260a22f18cf952f0f3fecb90b2f2ce5feccc4
The-Anh Vu-Le <vltanh@illinois.edu>
2024-05-15 10:28:31 -0500

    should not rerun the original

--- eval.sh
+++ eval.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --output=slurm_output/slurm-%j.out
 #SBATCH --job-name="eval"
 #SBATCH --partition=eng-research-gpu
 #SBATCH --mem=64G
-#SBATCH --dependency=afterok:11131610
 
 # ===================================
 
@@ -13,26 +12,27 @@ T=9
 
 for based_on in leiden_cpm_cm #leiden_cpm_cm leiden_cpm
 do
-    for network_id in cit_hepph cit_patents wiki_talk wiki_topcats orkut #cit_hepph cit_patents wiki_topcats wiki_talk orkut
+    for network_id in cit_hepph cit_patents wiki_talk #cit_hepph cit_patents wiki_topcats wiki_talk orkut
     do
         for resolution in .0001 .001 .01 #.0001 .001 .01
         do
+            orig_dir="data/networks/orig/${based_on}/${network_id}/leiden${resolution}/"
+            orig_outdir="data/stats/orig/${based_on}/${network_id}/leiden${resolution}/"
+
+            echo "=================================================="
+            echo $orig_dir
+
+            # python network_evaluation/compute_stats.py \
+            #         --input-network ${orig_dir}/edge.dat \
+            #         --input-clustering ${orig_dir}/com.dat \
+            #         --output-folder ${orig_outdir} \
+            #         --overwrite
+
             for method in abcdta4 abcd #abcd abcdta4
             do
-                orig_dir="data/networks/orig/${based_on}/${network_id}/leiden${resolution}/"
-                orig_outdir="data/stats/orig/${based_on}/${network_id}/leiden${resolution}/"
-
-                echo "=================================================="
-                echo $orig_dir
-
-                python network_evaluation/compute_stats.py \
-                        --input-network ${orig_dir}/edge.dat \
-                        --input-clustering ${orig_dir}/com.dat \
-                        --output-folder ${orig_outdir} \
-                        --overwrite
-
                 reps_dir="data/networks/${method}/${based_on}/${network_id}/leiden${resolution}/"
-                for seed in 0 #$(seq 1 $T)
+
+                for seed in $(seq 1 $T)
                 do
                     dir="${reps_dir}/${seed}/"
 
