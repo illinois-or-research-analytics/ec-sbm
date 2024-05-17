ae8e7e8234192220693933061be1a6588d1a3eef
The-Anh Vu-Le <vltanh@illinois.edu>
2024-05-17 08:59:28 -0500

    maybe cleaner

--- gen_abcdta4.py
+++ gen_abcdta4.py
@@ -23,6 +23,9 @@ clustering_fn = args.clustering
 output_dir = args.output_folder
 seed = args.seed
 
+logs = []
+
+start = time.perf_counter()
 set_up(
     edgelist_fn,
     clustering_fn,
@@ -30,28 +33,36 @@ set_up(
     output_dir,
     use_existing_clustering=True,
 )
+elapsed = time.perf_counter() - start
+logs.append(f"Setup time: {elapsed}")
 
 with open(f'{output_dir}/params.json', 'r') as f:
     params = json.load(f)
     seed = params['seed']
     xi = params['xi']
 
-# == Generate ABCD network ==
-print(f'Seed: {seed}')
+cmd = f'''
+julia ABCDGraphGenerator.jl/utils/graph_sampler_abcdta4.jl \\
+    {output_dir}/{EDGE} {output_dir}/{COM_OUT} \\
+    {output_dir}/{DEG} {output_dir}/{CS} \\
+    xi {xi} false false {seed} 0 \\
+    {output_dir}/{COM_INP} {output_dir}/{MCS}
+'''
+logs.append(cmd)
 
-cmd = f'julia ABCDGraphGenerator.jl/utils/graph_sampler_abcdta4.jl \
-                {output_dir}/{EDGE} {output_dir}/{COM_OUT} \
-                {output_dir}/{DEG} {output_dir}/{CS} \
-                xi {xi} false false {seed} 0 \
-                {output_dir}/{COM_INP} {output_dir}/{MCS}'
+start = time.perf_counter()
+os.system(cmd)
+elapsed = time.perf_counter() - start
+logs.append(f"Generation time: {elapsed}")
 
-with open(f'{output_dir}/run.log', 'w') as f:
-    f.write(cmd)
-    f.write('\n')
+start = time.perf_counter()
+post_process(output_dir)
+elapsed = time.perf_counter() - start
+logs.append(f"Post-process time: {elapsed}")
 
-    start = time.perf_counter()
-    os.system(cmd)
-    post_process(output_dir)
-    elapsed = time.perf_counter() - start
-
-    f.write(f"Generation time: {elapsed}")
+assert os.path.exists(output_dir)
+log_f = open(f'{output_dir}/run.log', 'w')
+for log in logs:
+    log_f.write(log)
+    log_f.write('\n')
+log_f.close()
