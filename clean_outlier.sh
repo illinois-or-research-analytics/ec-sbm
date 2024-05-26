efcb64c1bdb9bcd0e01d709daa27497b481a82f0
The-Anh Vu-Le <vltanh@illinois.edu>
2024-05-26 11:01:04 -0500

    clean outlier from original network

---
+++ clean_outlier.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --output=slurm_output/slurm-%j.out
+#SBATCH --job-name="clean_outlier_test"
+#SBATCH --partition=tallis
+#SBATCH --mem=64G
+
+# network_id=cit_hepph
+# resolution=.001
+# based_on=leiden_cpm_cm
+
+# input_dir="data/networks/orig/${based_on}/${network_id}/leiden${resolution}/"
+# output_dir="data/networks/orig_wo_outliers/${based_on}/${network_id}/leiden${resolution}/"
+
+# python clean_outlier.py \
+#     --input-network ${input_dir}/edge.dat \
+#     --input-clustering ${input_dir}/com.dat \
+#     --output-folder ${output_dir}
+
+# ===================================
+
+for based_on in leiden_cpm_cm leiden_cpm #leiden_cpm_cm leiden_cpm
+do
+    for network_id in cit_hepph cit_patents wiki_topcats wiki_talk orkut cen #cit_hepph cit_patents wiki_topcats wiki_talk orkut
+    do
+        for resolution in .0001 .001 .01 .1 .5 #.0001 .001 .01 .1 .5
+        do
+            input_dir="data/networks/orig/${based_on}/${network_id}/leiden${resolution}/"
+            output_dir="data/networks/orig_wo_outliers/${based_on}/${network_id}/leiden${resolution}/"
+
+            echo "Cleaning outliers"
+            echo "Input: ${input_dir}"
+            echo "Output: ${output_dir}"
+
+            # python clean_outlier.py \
+            #     --input-network ${input_dir}/edge.dat \
+            #     --input-clustering ${input_dir}/com.dat \
+            #     --output-folder ${output_dir}
+
+            python test_clean_outlier.py \
+                --output-network ${output_dir}/edge.dat \
+                --output-clustering ${output_dir}/com.dat
+
+            echo "=================================="
+            echo ""
+        done
+    done
+done
