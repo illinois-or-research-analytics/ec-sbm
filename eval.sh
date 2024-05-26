2340438723195246cea02a4860c9aa22f843d398
The-Anh Vu-Le <vltanh@illinois.edu>
2024-05-26 08:48:11 -0500

    update new CLI input

--- eval.sh
+++ eval.sh
@@ -8,43 +8,57 @@
 
 # ===================================
 
-T=9
+T=2
 
 for based_on in leiden_cpm_cm #leiden_cpm_cm leiden_cpm
 do
-    for network_id in cit_hepph cit_patents wiki_talk #cit_hepph cit_patents wiki_topcats wiki_talk orkut
+    for network_id in cit_hepph #cit_hepph cit_patents wiki_topcats wiki_talk orkut
     do
-        for resolution in .0001 .001 .01 #.0001 .001 .01
+        for resolution in .0001 #.0001 .001 .01
         do
             orig_dir="data/networks/orig/${based_on}/${network_id}/leiden${resolution}/"
-            orig_outdir="data/stats/orig/${based_on}/${network_id}/leiden${resolution}/"
+            edgelist_fn="${orig_dir}/edge.dat"
+            clustering_fn="${orig_dir}/com.dat"
 
-            echo "=================================================="
             echo $orig_dir
 
-            # python network_evaluation/compute_stats.py \
-            #         --input-network ${orig_dir}/edge.dat \
-            #         --input-clustering ${orig_dir}/com.dat \
-            #         --output-folder ${orig_outdir} \
-            #         --overwrite
+            echo "============================================"
 
+            echo "Computing original stats"
+
+            orig_stats_outdir="data/stats/orig/${based_on}/${network_id}/leiden${resolution}/"
+            python network_evaluation/compute_stats.py \
+                --input-network ${edgelist_fn} \
+                --input-clustering ${clustering_fn} \
+                --output-folder ${orig_stats_outdir} \
+                --overwrite
+
+            echo "============================"
+            echo ""
+            
             for method in abcdta4 abcd #abcd abcdta4
-            do
+            do    
                 reps_dir="data/networks/${method}/${based_on}/${network_id}/leiden${resolution}/"
+                echo $reps_dir
 
-                for seed in $(seq 1 $T)
+                for seed in $(seq 1 ${T})
                 do
                     dir="${reps_dir}/${seed}/"
 
-                    echo "=================================="
+                    echo "============================"
                     echo $dir
 
-                    # python gen_${method}.py \
-                    #     --network-id ${network_id} \
-                    #     --resolution $resolution \
-                    #     --method ${method} \
-                    #     --based_on ${based_on} \
-                    #     --seed ${seed}
+                    echo "Generating network"
+
+                    python gen_${method}.py \
+                        --edgelist ${edgelist_fn} \
+                        --clustering ${clustering_fn} \
+                        --output-folder ${dir} \
+                        --seed ${seed}
+
+                    echo "============================"
+
+                    echo "Computing stats"
 
                     python network_evaluation/compute_stats.py \
                         --input-network ${dir}/edge.tsv \
@@ -52,18 +66,28 @@ do
                         --output-folder ${dir} \
                         --overwrite
 
-                    echo "=================================="
-                    echo ""
+                    echo "============================"
+
+                    echo "Comparing with original"
+
+                    if [ $method = "abcdta4" ]; then
+                        python network_evaluation/compare_stats_pair.py \
+                            --network-1-folder ${orig_stats_outdir} \
+                            --network-2-folder ${dir} \
+                            --output-file ${dir}/compare_output.csv \
+                            --is-compare-sequence
+                    else
+                        python network_evaluation/compare_stats_pair.py \
+                            --network-1-folder ${orig_stats_outdir} \
+                            --network-2-folder ${dir} \
+                            --output-file ${dir}/compare_output.csv
+                    fi
                 done
-
-                python network_evaluation/compare_stats.py \
-                    --input-network-folder ${orig_outdir} \
-                    --input-replicates-folder ${reps_dir} \
-                    --output-folder ${reps_dir}
-
-                echo "=================================================="
+                echo "============================"
                 echo ""
             done
+            echo "============================================"
+            echo ""
         done
     done
 done
