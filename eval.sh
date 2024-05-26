4deb8363a8c59d9a036ea680d51cc821fd1f17fb
The-Anh Vu-Le <vltanh@illinois.edu>
2024-05-26 11:02:00 -0500

    full pipeline of evaluation

--- eval.sh
+++ eval.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
-#SBATCH --time=48:00:00
+#SBATCH --time=168:00:00
 #SBATCH --nodes=1
 #SBATCH --output=slurm_output/slurm-%j.out
 #SBATCH --job-name="eval"
-#SBATCH --partition=eng-research-gpu
+#SBATCH --partition=tallis
 #SBATCH --mem=64G
 
 # ===================================
 
-T=2
+orig="orig_wo_outliers"
+T=0
 
 for based_on in leiden_cpm_cm #leiden_cpm_cm leiden_cpm
 do
-    for network_id in cit_hepph #cit_hepph cit_patents wiki_topcats wiki_talk orkut
+    for network_id in cit_hepph cit_patents wiki_talk orkut wiki_topcats cen #cit_hepph cit_patents wiki_topcats wiki_talk orkut
     do
-        for resolution in .0001 #.0001 .001 .01
+        for resolution in .0001 .001 .01 #.0001 .001 .01
         do
-            orig_dir="data/networks/orig/${based_on}/${network_id}/leiden${resolution}/"
+            orig_dir="data/networks/${orig}/${based_on}/${network_id}/leiden${resolution}/"
+
             edgelist_fn="${orig_dir}/edge.dat"
             clustering_fn="${orig_dir}/com.dat"
 
-            echo $orig_dir
+            echo ${orig_dir}
+            echo "============================================"
+
+            # raw_dir="data/networks/orig/${based_on}/${network_id}/leiden${resolution}/"
+
+            # echo "Cleaning outliers"
+            # echo "Raw: ${raw_dir}"
+
+            # python clean_outlier.py \
+            #     --input-network ${raw_dir}/edge.dat \
+            #     --input-clustering ${raw_dir}/com.dat \
+            #     --output-folder ${orig_dir}
+
+            # python test_clean_outlier.py \
+            #     --output-network ${output_dir}/edge.dat \
+            #     --output-clustering ${output_dir}/com.dat
 
             echo "============================================"
 
             echo "Computing original stats"
 
-            orig_stats_outdir="data/stats/orig/${based_on}/${network_id}/leiden${resolution}/"
+            orig_stats_outdir="data/stats/${orig}/${based_on}/${network_id}/leiden${resolution}/"
             python network_evaluation/compute_stats.py \
                 --input-network ${edgelist_fn} \
                 --input-clustering ${clustering_fn} \
@@ -41,7 +58,7 @@ do
                 reps_dir="data/networks/${method}/${based_on}/${network_id}/leiden${resolution}/"
                 echo $reps_dir
 
-                for seed in $(seq 1 ${T})
+                for seed in $(seq 0 ${T})
                 do
                     dir="${reps_dir}/${seed}/"
 
@@ -66,22 +83,47 @@ do
                         --output-folder ${dir} \
                         --overwrite
 
+                    python compute_connectivity.py \
+                        --network-folder ${dir} \
+                        --output-folder ${dir}
+
+                    python compute_degree_dist.py \
+                        --network-folder ${dir} \
+                        --output-folder ${dir}
+
+                    if [ $method = "abcdta4" ]; then
+                        python compute_cluster_stats.py \
+                            --network-folder ${dir} \
+                            --output-folder ${dir} \
+                            --is-with-bijection
+                    else
+                        python compute_cluster_stats.py \
+                            --network-folder ${dir} \
+                            --output-folder ${dir}
+                    fi
+
                     echo "============================"
 
                     echo "Comparing with original"
 
-                    if [ $method = "abcdta4" ]; then
-                        python network_evaluation/compare_stats_pair.py \
+                    python network_evaluation/compare_stats_pair.py \
                             --network-1-folder ${orig_stats_outdir} \
                             --network-2-folder ${dir} \
                             --output-file ${dir}/compare_output.csv \
                             --is-compare-sequence
-                    else
-                        python network_evaluation/compare_stats_pair.py \
-                            --network-1-folder ${orig_stats_outdir} \
-                            --network-2-folder ${dir} \
-                            --output-file ${dir}/compare_output.csv
-                    fi
+
+                    # if [ $method = "abcdta4" ]; then
+                    #     python network_evaluation/compare_stats_pair.py \
+                    #         --network-1-folder ${orig_stats_outdir} \
+                    #         --network-2-folder ${dir} \
+                    #         --output-file ${dir}/compare_output.csv \
+                    #         --is-compare-sequence
+                    # else
+                    #     python network_evaluation/compare_stats_pair.py \
+                    #         --network-1-folder ${orig_stats_outdir} \
+                    #         --network-2-folder ${dir} \
+                    #         --output-file ${dir}/compare_output.csv
+                    # fi
                 done
                 echo "============================"
                 echo ""
