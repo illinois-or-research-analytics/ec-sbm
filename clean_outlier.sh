6b2abb67a031afef96990902dea751255f2cf692
The-Anh Vu-Le <vltanh@illinois.edu>
2024-05-30 15:43:29 -0500

    work with 104 networks

--- clean_outlier.sh
+++ clean_outlier.sh
@@ -20,11 +20,46 @@
 
 # ===================================
 
+# for based_on in leiden_cpm_cm leiden_cpm #leiden_cpm_cm leiden_cpm
+# do
+#     for network_id in cit_hepph cit_patents wiki_topcats wiki_talk orkut cen #cit_hepph cit_patents wiki_topcats wiki_talk orkut
+#     do
+#         for resolution in .0001 .001 .01 .1 .5 #.0001 .001 .01 .1 .5
+#         do
+#             input_dir="data/networks/orig/${based_on}/${network_id}/leiden${resolution}/"
+#             output_dir="data/networks/orig_wo_outliers/${based_on}/${network_id}/leiden${resolution}/"
+
+#             echo "Cleaning outliers"
+#             echo "Input: ${input_dir}"
+#             echo "Output: ${output_dir}"
+
+#             python clean_outlier.py \
+#                 --input-network ${input_dir}/edge.dat \
+#                 --input-clustering ${input_dir}/com.dat \
+#                 --output-folder ${output_dir}
+
+#             python test_clean_outlier.py \
+#                 --output-network ${output_dir}/edge.dat \
+#                 --output-clustering ${output_dir}/com.dat
+
+#             echo "=================================="
+#             echo ""
+#         done
+#     done
+# done
+
+# ===================================
+
 for based_on in leiden_cpm_cm leiden_cpm #leiden_cpm_cm leiden_cpm
 do
-    for network_id in cit_hepph cit_patents wiki_topcats wiki_talk orkut cen #cit_hepph cit_patents wiki_topcats wiki_talk orkut
+    for network_id in $(cat data/networks.txt)
     do
-        for resolution in .0001 .001 .01 .1 .5 #.0001 .001 .01 .1 .5
+        # Ignore lines starting with #
+        if [[ $network_id == \#* ]]; then
+            continue
+        fi
+
+        for resolution in .001 #.0001 .001 .01 .1 .5
         do
             input_dir="data/networks/orig/${based_on}/${network_id}/leiden${resolution}/"
             output_dir="data/networks/orig_wo_outliers/${based_on}/${network_id}/leiden${resolution}/"
@@ -33,10 +68,10 @@ do
             echo "Input: ${input_dir}"
             echo "Output: ${output_dir}"
 
-            # python clean_outlier.py \
-            #     --input-network ${input_dir}/edge.dat \
-            #     --input-clustering ${input_dir}/com.dat \
-            #     --output-folder ${output_dir}
+            python clean_outlier.py \
+                --input-network ${input_dir}/edge.dat \
+                --input-clustering ${input_dir}/com.dat \
+                --output-folder ${output_dir}
 
             python test_clean_outlier.py \
                 --output-network ${output_dir}/edge.dat \
