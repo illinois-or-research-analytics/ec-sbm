a7d3042e4788032b38fc1569d7b974569e344226
The-Anh Vu-Le <vltanh@illinois.edu>
2024-06-05 16:49:21 -0500

    update

---
+++ compare_simulators.sh
@@ -0,0 +1,4 @@
+python network_evaluation/compare_simulators.py \
+    --names SBM ABCD \
+    --roots data/networks/sbm/leiden_cpm_cm data/networks/abcd/leiden_cpm_cm \
+    --output-dir output/sbm_abcd/
