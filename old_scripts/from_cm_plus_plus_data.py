e01056b9207a17449f8389817ec0ee64bf301bba
The-Anh Vu-Le <vltanh@illinois.edu>
2024-10-21 10:24:39 -0500

    minor changes

--- old_scripts/from_cm_plus_plus_data.py
+++ old_scripts/from_cm_plus_plus_data.py
@@ -41,48 +41,51 @@ for file in S1_files:
 
     edgelist_files[network_id] = file
 
-# S2_files = pathlib.Path(root).rglob('S2_*.tsv')
-# for file in S2_files:
-#     fn = file.stem
-
-#     network_id = fn.split('-')[0].replace('S2_', '')
-
-#     if network_id not in NETWORK_IDS:
-#         continue
-
-#     r = fn.split('-')[1].split('_')[0][1:]
-
-#     if r not in RS:
-#         continue
-
-#     print('S2', network_id, r)
-#     # output_folder = f'{output_root}/leiden_cpm/{network_id}/leiden{r}/'
-#     # os.makedirs(output_folder, exist_ok=True)
-#     # os.system(f'cp {edgelist_files[network_id]} {output_folder}/edge.dat')
-#     # os.system(f'cp {file} {output_folder}/com.dat')
-
-S5_files = pathlib.Path(root).rglob('S5_*.tsv')
-for file in S5_files:
+S2_files = pathlib.Path(root).rglob('S2_*.tsv')
+for file in S2_files:
     fn = file.stem
 
-    network_id = fn.split('-')[0].replace('S5_', '').replace('_ikc.connectivity_modifier_k10', '')
+    network_id = fn.split('-')[0].replace('S2_', '')
 
     if network_id not in NETWORK_IDS:
         continue
 
     r = fn.split('-')[1].split('_')[0]
+
+    if r[0] == '0':
+        r = r[1:]
+
     if r not in RS:
         continue
 
-    print('S5', network_id, r)
-
-    output_folder = f'{output_root}/leiden_mod_cm/{network_id}/leiden{r}/'
+    print('S2', network_id, r)
+    output_folder = f'{output_root}/leiden_mod/{network_id}/leiden{r}/'
     os.makedirs(output_folder, exist_ok=True)
     os.system(f'cp {edgelist_files[network_id]} {output_folder}/edge.dat')
     os.system(f'cp {file} {output_folder}/com.dat')
 
-    # os.system(
-    #     f'Rscript cm_pipeline/scripts/post_cm_filter.R {file} {output_folder}/com.tsv')
+# S5_files = pathlib.Path(root).rglob('S5_*.tsv')
+# for file in S5_files:
+#     fn = file.stem
+
+#     network_id = fn.split('-')[0].replace('S5_', '').replace('_ikc.connectivity_modifier_k10', '')
+
+#     if network_id not in NETWORK_IDS:
+#         continue
+
+#     r = fn.split('-')[1].split('_')[0]
+#     if r not in RS:
+#         continue
+
+#     print('S5', network_id, r)
+
+#     output_folder = f'{output_root}/leiden_mod_cm/{network_id}/leiden{r}/'
+#     os.makedirs(output_folder, exist_ok=True)
+#     os.system(f'cp {edgelist_files[network_id]} {output_folder}/edge.dat')
+#     os.system(f'cp {file} {output_folder}/com.dat')
+
+#     # os.system(
+#     #     f'Rscript cm_pipeline/scripts/post_cm_filter.R {file} {output_folder}/com.tsv')
 
 # Check result
 for network_id in NETWORK_IDS:
@@ -93,8 +96,8 @@ for network_id in NETWORK_IDS:
         # if not os.path.exists(f'{output_root}/leiden_cpm/{network_id}/leiden{r}/com.dat'):
         #     print(f'{network_id} {r} S2 not found')
 
-        if not os.path.exists(f'{output_root}/leiden_mod_cm/{network_id}/leiden{r}/edge.dat'):
+        if not os.path.exists(f'{output_root}/leiden_mod/{network_id}/leiden{r}/edge.dat'):
             print(f'{network_id} {r} S1 not found')
 
-        if not os.path.exists(f'{output_root}/leiden_mod_cm/{network_id}/leiden{r}/com.dat'):
-            print(f'{network_id} {r} S5 not found')
+        if not os.path.exists(f'{output_root}/leiden_mod/{network_id}/leiden{r}/com.dat'):
+            print(f'{network_id} {r} S2 not found')
