00cb165f3c0d30ed257851af6cf1b05632e10cf0
The-Anh Vu-Le <vltanh@illinois.edu>
2024-12-13 17:54:36 -0600

    compute graph-tool stats

---
+++ compute_gt_stats.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#SBATCH --time=5-00:00:00
+#SBATCH --nodes=1
+#SBATCH --output=slurm_output/compute_gt_stats/slurm-%j.out
+#SBATCH --job-name="gtstats_abcdmcs+eV1"
+#SBATCH --partition=tallis
+#SBATCH --mem=64G
+
+# ===================================
+
+start=0
+end=0
+
+for clustering in sbm sbm_cc sbm_wcc leiden_cpm leiden_cpm_nofiltcm leiden_mod leiden_mod_nofiltcm ikc_cc ikc_nofiltcm infomap_cc infomap_nofiltcm
+do
+    for resolution in sbm leiden.001 leiden.01 leiden.1 leidenmod k10 infomap
+    do
+        # Matching clustering with resolution
+        if [ $clustering = "leiden_cpm_cm" ] || [ $clustering = "leiden_cpm" ] || [ $clustering = "leiden_cpm_nofiltcm" ]; then
+            if [ ! $resolution = "leiden.001" ] && [ ! $resolution = "leiden.01" ] && [ ! $resolution = "leiden.1" ]; then
+                continue
+            fi
+        elif [ $clustering = "leiden_mod_cm" ] || [ $clustering = "leiden_mod" ] || [ $clustering = "leiden_mod_nofiltcm" ]; then
+            if [ ! $resolution = "leidenmod" ]; then
+                continue
+            fi
+        elif [ $clustering = "ikc_cm" ] || [ $clustering = "ikc_cc" ] || [ $clustering = "ikc_nofiltcm" ]; then
+            if [ ! $resolution = "k10" ]; then
+                continue
+            fi
+        elif [ $clustering = "infomap_cc" ] || [ $clustering = "infomap_nofiltcm" ]; then
+            if [ ! $resolution = "infomap" ]; then
+                continue
+            fi
+        elif [ $clustering = "sbm_cc" ] || [ $clustering = "sbm_wcc" ] || [ $clustering = "sbm" ]; then
+            if [ ! $resolution = "sbm" ]; then
+                continue
+            fi
+        fi
+        
+        for network_id in $(cat data/networks_val.txt) # $(cat data/networks_train.txt) $(cat data/networks_val.txt) $(cat data/networks_test.txt)
+        do  
+            for method in abcdta4+o+eL1 #abcd abcdta4 sbm sbmmcspres sbmmcsprev1
+            do
+                input_dirs="data/networks/${method}/${clustering}/${network_id}/${resolution}/"
+                output_stat_dirs="data/stats/${method}/${clustering}/${network_id}/${resolution}/"
+
+                for seed in $(seq ${start} ${end})
+                do
+                    echo "============================"
+                    input_dir="${input_dirs}/${seed}/"
+                    output_stat_dir="${output_stat_dirs}/${seed}/"
+                    echo ${input_dir}
+
+                    if [ ! -f ${input_dir}/edge.tsv ]; then
+                        echo "Error: ${input_dir}/edge.tsv not found"
+                        continue
+                    fi
+
+                    python network_evaluation/compute_gt_stats.py \
+                        --input ${input_dir}/edge.tsv \
+                        --output ${output_stat_dir}
+                done
+                echo "============================"
+                echo ""
+            done
+            echo "============================================"
+            echo ""
+        done
+    done
+done
