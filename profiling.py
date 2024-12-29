c8284ac3469517892affb8eab2a38dc37f7b7dfe
The-Anh Vu-Le <vltanh@illinois.edu>
2024-12-29 00:38:39 -0600

    fix time

--- profiling.py
+++ profiling.py
@@ -3,8 +3,9 @@ import re
 
 import pandas as pd
 
+split = 'val_small'
 root = Path('data/networks/')
-networks_list = 'data/networks_val.txt'
+networks_list = f'data/networks_{split}.txt'
 
 network_ids = [
     line.strip() for line in open(networks_list)
@@ -14,7 +15,8 @@ method = 'sbmmcsprev1'
 clustering = 'sbm_wcc'
 resolution = 'sbm'
 
-out_fp = Path(f'output/profiles/time_{method}_{clustering}_{resolution}.csv')
+out_fp = Path(
+    f'output/profiles/time_{split}_{method}_{clustering}_{resolution}.csv')
 out_fp.parent.mkdir(parents=True, exist_ok=True)
 
 all_profiles = []
@@ -136,22 +138,43 @@ for network_id in network_ids:
         lines = list(f.readlines())
 
     profile_mapping = {
-        'o_repl_cluster': 'INFO - Replicate clustering: ',
-        'o_combine': 'INFO - Combine clustered and outlier subgraphs: ',
+        'e_repl_cluster': 'INFO - Replicate clustering: ',
+        'e_combine': 'INFO - Combine clustered and outlier subgraphs: ',
     }
+
+    for line in lines:
+        for key, value in profile_mapping.items():
+            if value in line:
+                profile[key] = float(line.split(value)[1].strip())
 
     all_profiles.append(profile)
 
 df = pd.DataFrame(all_profiles)
-df['c_total'] = df['c_setup'] + df['c_gen_kec'] + \
-    df['c_compute_sbm'] + df['c_gen_remaining'] + df['c_post']
-df['o_total'] = df['o_setup'] + df['o_gen'] + df['o_post']
-df['o_combine_total'] = df['o_repl_cluster'] + df['o_combine']
-df['e_total'] = df['e_process_cluster'] + df['e_process_edgelist'] + df['e_create_outlier_cluster'] + \
+df['c_total'] = \
+    df['c_setup'] + \
+    df['c_gen_kec'] + \
+    df['c_compute_sbm'] + \
+    df['c_gen_remaining'] + \
+    df['c_post']
+df['o_total'] = \
+    df['o_setup'] + \
+    df['o_gen'] + \
+    df['o_post'] + \
+    df['o_combine'] + \
+    df['o_repl_cluster']
+df['e_total'] = \
+    df['e_process_cluster'] + \
+    df['e_process_edgelist'] + \
+    df['e_create_outlier_cluster'] + \
     df['e_compute_sbm_original'] + \
-    df['e_update_sbm_existing'] + df['e_add_edge'] + df['e_post_process']
-df['e_combine_total'] = df['o_repl_cluster'] + df['o_combine']
-df['total'] = df['c_total'] + df['o_total'] + \
-    df['o_combine_total'] + df['e_total'] + df['e_combine_total']
+    df['e_update_sbm_existing'] + \
+    df['e_add_edge'] + \
+    df['e_post_process'] + \
+    df['e_combine'] + \
+    df['e_repl_cluster']
+df['total'] = \
+    df['c_total'] + \
+    df['o_total'] + \
+    df['e_total']
 df.sort_values('total', inplace=True, ascending=False)
 df.to_csv(str(out_fp), index=False)
