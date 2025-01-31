67bb6c3af61c385d1e2ee09b6096834fadc206bd
The-Anh Vu-Le <vltanh@illinois.edu>
2025-01-31 11:27:58 -0600

    side project: entropy checking

--- misc/check_entropy.py
+++ misc/check_entropy.py
@@ -2,43 +2,155 @@ import json
 import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
+import argparse
+from pathlib import Path
+import pandas as pd
 
 matplotlib.rcParams.update({'font.size': 18})
 
-input_fp = 'test.json'
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', type=str,
+                    default='test.json', help='Input JSON file')
+parser.add_argument('--whitelist', type=str,
+                    default=None, help='List of networks to include')
+parser.add_argument('--blacklist', type=str,
+                    default=None, help='List of networks to exclude')
+parser.add_argument('--output', type=str,
+                    default='output/entropy/', help='Output directory')
+args = parser.parse_args()
+
+# Create output directory
+output_dir = Path(args.output)
+output_dir.mkdir(parents=True, exist_ok=True)
+
+# Load list of whitelist networks
+networks_id = [
+    line.strip()
+    for line in open(args.whitelist, 'r').readlines()
+] if args.whitelist else None
+
+# Load list of blacklist networks
+blacklist = [
+    line.strip()
+    for line in open(args.blacklist, 'r').readlines()
+] if args.blacklist else None
+
+# Load data
+input_fp = args.input
 with open(input_fp, 'r') as f:
     data = json.load(f)
+
+# Save list of networks
+with open(output_dir / 'orig_networks.txt', 'w') as f:
+    f.write('\n'.join(data.keys()))
+
+# Filter out network ids
+data = {
+    network_id: data[network_id]
+    for network_id in data.keys()
+    if (networks_id is None or network_id in networks_id) and (blacklist is None or network_id not in blacklist)
+}
+
+# Save list of networks
+with open(output_dir / 'networks.txt', 'w') as f:
+    f.write('\n'.join(data.keys()))
 
 # # check if - log P(A|e,b,k) for dccc is <= - log P(A|e,b,k) for dc
 # for network_id, network_data in data.items():
 #     if network_data['dccc']['-log P(A|e,b,k)'] > network_data['dc']['-log P(A|e,b,k)']:
-#         print(f"Network {network_id} has higher - log P(A|e,b,k) for dccc than dc")
+#         print(
+#             f"Network {network_id} has higher - log P(A|e,b,k) for dccc than dc")
 
-# # check if without - log P(e) for dccc is <= - log P(e) for dc
-# list_of_networks = []
-# for network_id, network_data in data.items():
-#     dccc = network_data['dccc']['-log P(A|e,b,k)'] + network_data['dccc']['-log P(k|e,b)'] + network_data['dccc']['-log P(e)']
-#     dc = network_data['dc']['-log P(A|e,b,k)'] + network_data['dc']['-log P(k|e,b)'] + network_data['dc']['-log P(e)']
-#     if dccc > dc:
-#         list_of_networks.append(network_id)
-#         print(f"Network {network_id} has higher DL for dccc than dc without - log P(e)")
-# print(f"Total networks with higher DL for dccc than dc without - log P(e): {len(list_of_networks)}")
-# print(f"Ratio: {len(list_of_networks) / len(data)}")
-# print(f"Networks: {list_of_networks}")
-
-# check if the sum matches
+# check if without - log P(e) for dccc is <= - log P(e) for dc
+all_networks = set(data.keys())
+special_networks = set()
 for network_id, network_data in data.items():
-    print(f"== Network {network_id}")
+    dccc = network_data['dccc']['-log P(A|e,b,k)'] + \
+        network_data['dccc']['-log P(k|e,b)'] + \
+        network_data['dccc']['-log P(b)']
+    dc = network_data['dc']['-log P(A|e,b,k)'] + \
+        network_data['dc']['-log P(k|e,b)'] + network_data['dc']['-log P(b)']
+    if dccc == dc:
+        special_networks.add(network_id)
+        print(
+            f"Network {network_id} has DL for dccc <= dc without - log P(e): {dccc} <= {dc}")
+print(
+    f"(A) Networks with higher DL for dccc than dc without -log P(e): {len(special_networks)}")
+print(f"Total networks: {len(data)}")
+print(f"Ratio: {len(special_networks) / len(data)}")
+print(f"Networks - (A): {special_networks}")
 
-    dccc = network_data['dccc']['-log P(A|e,b,k)'] + network_data['dccc']['-log P(k|e,b)'] + network_data['dccc']['-log P(e)'] + network_data['dccc']['-log P(b)']
-    dccc_dl = network_data['dccc']['-log P(A,e,b,k)']
-    print(f"Diff: {dccc - dccc_dl}")
+# Gather the differences in entropy between dccc and dc for each component
+df_data = []
+for network_id, network_data in data.items():
+    PAebk_dccc = network_data['dccc']['-log P(A|e,b,k)']
+    PAebk_dc = network_data['dc']['-log P(A|e,b,k)']
+    Pkeb_dccc = network_data['dccc']['-log P(k|e,b)']
+    Pkeb_dc = network_data['dc']['-log P(k|e,b)']
+    Pe_dccc = network_data['dccc']['-log P(e)']
+    Pe_dc = network_data['dc']['-log P(e)']
+    Pb_dccc = network_data['dccc']['-log P(b)']
+    Pb_dc = network_data['dc']['-log P(b)']
 
-    dc = network_data['dc']['-log P(A|e,b,k)'] + network_data['dc']['-log P(k|e,b)'] + network_data['dc']['-log P(e)'] + network_data['dc']['-log P(b)']
-    dc_dl = network_data['dc']['-log P(A,e,b,k)']
-    print(f"Diff: {dc - dc_dl}")
+    df_data.append({
+        'Network': network_id,
+        'Component': '-log P(A|e,b,k)',
+        'Difference': PAebk_dccc - PAebk_dc
+    })
 
-    input()
+    df_data.append({
+        'Network': network_id,
+        'Component': '-log P(k|e,b)',
+        'Difference': Pkeb_dccc - Pkeb_dc
+    })
+
+    df_data.append({
+        'Network': network_id,
+        'Component': '-log P(e)',
+        'Difference': Pe_dccc - Pe_dc
+    })
+
+    df_data.append({
+        'Network': network_id,
+        'Component': '-log P(b)',
+        'Difference': Pb_dccc - Pb_dc
+    })
+
+df = pd.DataFrame(df_data)
+
+# Make box plots
+fig, ax = plt.subplots(figsize=(10, 8), dpi=150)
+sns.boxplot(x='Component', y='Difference', data=df, ax=ax, showfliers=False)
+ax.set_xlabel('Component')
+ax.set_ylabel('Difference SBM(DC)-CC - SBM(DC)')
+ax.axhline(0, color='red', linestyle='--', linewidth=0.1)
+plt.tight_layout()
+plt.savefig(f'{output_dir}/dccc_vs_dc_entropy_diff_nofliers.pdf')
+
+# Make box plots
+fig, ax = plt.subplots(figsize=(10, 8), dpi=150)
+sns.boxplot(x='Component', y='Difference', data=df, ax=ax)
+ax.set_xlabel('Component')
+ax.set_ylabel('Difference SBM(DC)-CC - SBM(DC)')
+ax.axhline(0, color='red', linestyle='--', linewidth=0.1)
+plt.tight_layout()
+plt.savefig(f'{output_dir}/dccc_vs_dc_entropy_diff.eps')
+
+# # check if the sum matches
+# for network_id, network_data in data.items():
+#     print(f"== Network {network_id}")
+
+#     dccc = network_data['dccc']['-log P(A|e,b,k)'] + network_data['dccc']['-log P(k|e,b)'] + \
+#         network_data['dccc']['-log P(e)'] + network_data['dccc']['-log P(b)']
+#     dccc_dl = network_data['dccc']['-log P(A,e,b,k)']
+#     print(f"Diff: {dccc - dccc_dl}")
+
+#     dc = network_data['dc']['-log P(A|e,b,k)'] + network_data['dc']['-log P(k|e,b)'] + \
+#         network_data['dc']['-log P(e)'] + network_data['dc']['-log P(b)']
+#     dc_dl = network_data['dc']['-log P(A,e,b,k)']
+#     print(f"Diff: {dc - dc_dl}")
+
+#     input()
 
 # # plot - log P(A|e,b,k) for dccc vs dc
 # dccc_adjacency_entropy = [network_data['dccc']['-log P(A|e,b,k)'] for network_data in data.values()]
@@ -67,10 +179,12 @@ for network_id, network_data in data.items():
 #         print(f"Network {network_id} has - log P(A|e,b,k) for dccc equal to 0")
 #         # continue
 #     if network_data['dccc']['-log P(A|e,b,k)'] == network_data['dc']['-log P(A|e,b,k)']:
-#         print(f"Network {network_id} has - log P(A|e,b,k) for dccc equal to dc")
+#         print(
+#             f"Network {network_id} has - log P(A|e,b,k) for dccc equal to dc")
 #         # continue
 #     ratio.append(
-#         network_data['dccc']['-log P(A|e,b,k)'] / network_data['dc']['-log P(A|e,b,k)']
+#         network_data['dccc']['-log P(A|e,b,k)'] /
+#         network_data['dc']['-log P(A|e,b,k)']
 #         if network_data['dc']['-log P(A|e,b,k)'] != 0 and network_data['dccc']['-log P(A|e,b,k)'] != 0
 #         else 1.0
 #     )
@@ -80,7 +194,7 @@ for network_id, network_data in data.items():
 # ax.set_ylabel('Frequency')
 # ax.set_xlim(0)
 # plt.tight_layout()
-# plt.savefig('dccc_vs_dc_A_ratio.pdf')
+# plt.savefig(f'{output_dir}/dccc_vs_dc_A_ratio.pdf')
 
 # # plot ratio of - log P(k|e,b) for DC+CC to DC
 # ratio = []
